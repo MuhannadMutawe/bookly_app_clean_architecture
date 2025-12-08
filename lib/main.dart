e345e85e206d6c32bd92386ad926cc1fb82b199a
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-08 14:50:26 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_clean_arch/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
