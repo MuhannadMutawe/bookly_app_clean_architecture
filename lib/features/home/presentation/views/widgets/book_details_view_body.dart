eaafa5418ed5f0983735b62e4810d879e4d9749e
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-15 18:19:41 +0200

    create book details view app bar & add it to book details view body.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_clean_arch/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,7 +7,9 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: [
+        CustomBookDetailsAppBar(),
+      ],
     );
   }
 }
