a6ba1668b34790246333a4d1ce39e866215c624a
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-25 19:29:23 +0200

    fetch newest books use case.

---
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_cases.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_clean_arch/core/errors/failure.dart';
+import 'package:bookly_clean_arch/core/use_case/use_cases.dart';
+import 'package:bookly_clean_arch/features/home/domain/entities/book_entities.dart';
+import 'package:bookly_clean_arch/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCases extends UseCase<List<BookEntities>> {
+  final HomeRepo _homeRepo;
+
+  FetchNewestBooksUseCases(this._homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntities>>> call() async {
+    // TODO: implement call
+    return await _homeRepo.fetchNewestBooks();
+  }
+}
