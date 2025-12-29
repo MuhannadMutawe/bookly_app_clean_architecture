50cd38dd2a21806528d72741fc038915dcfd545a
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-29 18:04:08 +0200

    create home remote data source.

---
+++ lib/features/home/data/data_source/home_remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_clean_arch/features/home/domain/entities/book_entities.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImplementaion extends HomeRemoteDataSource {
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
