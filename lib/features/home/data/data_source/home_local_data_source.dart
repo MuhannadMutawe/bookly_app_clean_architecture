11ed687066af0725734a4d2850588aaed954554d
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-30 13:36:27 +0200

    finished fetch featured books method to local data source.

--- lib/features/home/data/data_source/home_local_data_source.dart
+++ lib/features/home/data/data_source/home_local_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_clean_arch/constants.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks();
@@ -8,8 +10,8 @@ abstract class HomeLocalDataSource {
 class HomeLocalDataSourceImplementation extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
