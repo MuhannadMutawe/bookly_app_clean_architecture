1f5829b722ee0cd666c490cb3a395b5e08c943be
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-31 20:18:31 +0200

    setup service locator.

---
+++ lib/core/utils/functions/setup_service_locator.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_clean_arch/core/utils/api_service.dart';
+import 'package:bookly_clean_arch/features/home/data/data_source/home_local_data_source.dart';
+import 'package:bookly_clean_arch/features/home/data/data_source/home_remote_data_source.dart';
+import 'package:bookly_clean_arch/features/home/data/repos/home_repo_implementation.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<HomeRepoImplementation>(
+    HomeRepoImplementation(
+      homeLocalDataSource: HomeLocalDataSourceImplementation(),
+      homeRemoteDataSource: HomeRemoteDataSourceImplementation(
+        ApiService(
+          Dio(),
+        ),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:bookly_clean_arch/constants.dart';
-import 'package:bookly_clean_arch/core/utils/api_service.dart';
 import 'package:bookly_clean_arch/core/utils/app_router.dart';
-import 'package:bookly_clean_arch/features/home/data/data_source/home_local_data_source.dart';
-import 'package:bookly_clean_arch/features/home/data/data_source/home_remote_data_source.dart';
+import 'package:bookly_clean_arch/core/utils/functions/setup_service_locator.dart';
 import 'package:bookly_clean_arch/features/home/data/repos/home_repo_implementation.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/features/home/domain/use_cases/fetch_featured_books_use_cases.dart';
 import 'package:bookly_clean_arch/features/home/domain/use_cases/fetch_newest_books_use_cases.dart';
 import 'package:bookly_clean_arch/features/home/presentation/manger/cubit/newest_books_cubit.dart';
 import 'package:bookly_clean_arch/features/home/presentation/manger/featured_books/featured_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,6 +17,7 @@ void main() async {
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
+  setupServiceLocator();
   runApp(const MyApp());
 }
 
@@ -33,28 +31,14 @@ class MyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
             FetchFeaturedBooksUseCases(
-              HomeRepoImplementation(
-                homeLocalDataSource: HomeLocalDataSourceImplementation(),
-                homeRemoteDataSource: HomeRemoteDataSourceImplementation(
-                  ApiService(
-                    Dio(),
-                  ),
-                ),
-              ),
+              getIt.get<HomeRepoImplementation>(),
             ),
           ),
         ),
         BlocProvider(
           create: (context) => NewestBooksCubit(
             FetchNewestBooksUseCases(
-              HomeRepoImplementation(
-                homeLocalDataSource: HomeLocalDataSourceImplementation(),
-                homeRemoteDataSource: HomeRemoteDataSourceImplementation(
-                  (ApiService(
-                    Dio(),
-                  )),
-                ),
-              ),
+              getIt.get<HomeRepoImplementation>(),
             ),
           ),
         ),
