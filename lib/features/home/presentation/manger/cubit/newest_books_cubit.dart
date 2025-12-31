9dab71fb6e14e7ef48224ddee4cba981347ac1ef
Muhannad R Mutawe <muhannadrm208@gmail.com>
2025-12-31 19:29:47 +0200

    implement for fetch newest books cubit method.

--- lib/features/home/presentation/manger/cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manger/cubit/newest_books_cubit.dart
@@ -1,6 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_clean_arch/features/home/domain/use_cases/fetch_newest_books_use_cases.dart';
 import 'package:bookly_clean_arch/features/home/presentation/manger/cubit/newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksState.initial());
+  NewestBooksCubit(this._fetchNewestBooksUseCases)
+    : super(NewestBooksState.initial());
+
+  final FetchNewestBooksUseCases _fetchNewestBooksUseCases;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksState.loading());
+    var result = await _fetchNewestBooksUseCases.call();
+    result.fold(
+      (failure) {
+        emit(NewestBooksState.failure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewestBooksState.success(books));
+      },
+    );
+  }
 }
