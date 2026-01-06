4c15e169c36da419818cbe986a823993090b50dd
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-01-06 16:28:04 +0200

    create newest books list view bloc builder & add it to home view body.

---
+++ lib/features/home/presentation/views/widgets/newest_books_list_view_bloc_builder.dart
@@ -0,0 +1,34 @@
+import 'package:bookly_clean_arch/features/home/presentation/manger/cubit/newest_books_cubit.dart';
+import 'package:bookly_clean_arch/features/home/presentation/manger/cubit/newest_books_state.dart';
+import 'package:bookly_clean_arch/features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart';
+import 'package:bookly_clean_arch/features/home/presentation/views/widgets/newest_books_list_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NewestBooksListViewBlocBuilder extends StatelessWidget {
+  const NewestBooksListViewBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        return state.when(
+          initial: () {
+            return SizedBox.shrink();
+          },
+          loading: () {
+            return FeaturedBooksListViewBlocBuilder();
+          },
+          success: (books) {
+            return NewestBooksListView(
+              books: books,
+            );
+          },
+          failure: (errorMassage) {
+            return ErrorWidget(errorMassage);
+          },
+        );
+      },
+    );
+  }
+}
