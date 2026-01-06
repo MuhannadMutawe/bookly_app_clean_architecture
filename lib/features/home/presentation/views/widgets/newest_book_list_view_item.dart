1178eb7fdc4b8cf1ad8c9bef4df0a8d0a9803c33
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-01-06 14:43:41 +0200

    display book data in newest list view item

--- lib/features/home/presentation/views/widgets/newest_book_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/newest_book_list_view_item.dart
@@ -2,6 +2,7 @@ import 'package:bookly_clean_arch/constants.dart';
 import 'package:bookly_clean_arch/core/utils/app_router.dart';
 import 'package:bookly_clean_arch/core/utils/assets.dart';
 import 'package:bookly_clean_arch/core/utils/styles.dart';
+import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_clean_arch/features/home/presentation/views/widgets/books_image_view_item.dart';
 import 'package:flutter/material.dart';
@@ -10,20 +11,23 @@ import 'package:go_router/go_router.dart';
 class NewestBookListViewItem extends StatelessWidget {
   const NewestBookListViewItem({
     super.key,
+    required this.book,
   });
+
+  final BookEntity book;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        context.push(AppRouter.kbookDetailsView);
+        context.push(AppRouter.kbookDetailsView, extra: book);
       },
       child: SizedBox(
         height: 130,
         child: Row(
           children: [
             BooksImageviewItem(
-              imageUrl: AssetsData.testImage,
+              imageUrl: book.image ?? AssetsData.testImage,
             ),
             const SizedBox(
               width: 30,
@@ -36,7 +40,7 @@ class NewestBookListViewItem extends StatelessWidget {
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.5,
                     child: Text(
-                      'bookModel.volumeInfo.title!',
+                      book.title,
                       maxLines: 2,
                       overflow: TextOverflow.ellipsis,
                       style: Styles.textStyle20.copyWith(
@@ -45,7 +49,7 @@ class NewestBookListViewItem extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    'bookModel.volumeInfo.authors![0]',
+                    book.authorName?[0] ?? '',
                     style: Styles.textStyle14,
                   ),
                   Row(
@@ -58,7 +62,7 @@ class NewestBookListViewItem extends StatelessWidget {
                       ),
                       Spacer(),
                       BookRating(
-                        rating: 0,
+                        rating: book.rating ?? 0,
                         count: 00,
                       ),
                     ],
--- lib/features/home/presentation/views/widgets/newest_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/newest_books_list_view.dart
@@ -1,20 +1,24 @@
+import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/features/home/presentation/views/widgets/newest_book_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class NewestBooksListView extends StatelessWidget {
-  const NewestBooksListView({super.key});
+  const NewestBooksListView({super.key, required this.books});
 
+  final List<BookEntity> books;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       padding: EdgeInsets.only(bottom: 25),
       physics: NeverScrollableScrollPhysics(),
       shrinkWrap: true,
-      itemCount: 25,
+      itemCount: books.length,
       itemBuilder: (context, index) {
         return Padding(
           padding: EdgeInsets.symmetric(vertical: 12),
-          child: NewestBookListViewItem(),
+          child: NewestBookListViewItem(
+            book: books[index],
+          ),
         );
       },
     );
