fa5466d9d61a60b6389f6400a90f26946f796956
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-01 12:42:28 +0200

    implement search remote data source.

--- lib/features/search/data/data_source/search_remote_data_source.dart
+++ lib/features/search/data/data_source/search_remote_data_source.dart
@@ -1,6 +1,31 @@
 import 'package:bookly_clean_arch/core/use_case/use_cases.dart';
+import 'package:bookly_clean_arch/core/utils/api_service.dart';
+import 'package:bookly_clean_arch/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 
 abstract class SearchRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks({NoParam? noParam});
+}
+
+class SearchRemoteDataSourceImplementation extends SearchRemoteDataSource {
+  final ApiService _apiService;
+
+  SearchRemoteDataSourceImplementation(this._apiService);
+
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks({NoParam? noParam}) async {
+    var data = await _apiService.get(
+      endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=programming',
+    );
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var item in data['items']) {
+      books.add(BookModel.fromJson(item));
+    }
+    return books;
+  }
 }
