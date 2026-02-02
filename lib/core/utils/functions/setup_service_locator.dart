40963578ae40cf791c5a2091e99175b298195553
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-02 21:44:38 +0200

    setup get_it for search repo implementation.

--- lib/core/utils/functions/setup_service_locator.dart
+++ lib/core/utils/functions/setup_service_locator.dart
@@ -2,6 +2,9 @@ import 'package:bookly_clean_arch/core/utils/api_service.dart';
 import 'package:bookly_clean_arch/features/home/data/data_source/home_local_data_source.dart';
 import 'package:bookly_clean_arch/features/home/data/data_source/home_remote_data_source.dart';
 import 'package:bookly_clean_arch/features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly_clean_arch/features/search/data/data_source/search_local_data_source.dart';
+import 'package:bookly_clean_arch/features/search/data/data_source/search_remote_data_source.dart';
+import 'package:bookly_clean_arch/features/search/data/repos/search_repo_implementation.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -12,6 +15,16 @@ void setupServiceLocator() {
     HomeRepoImplementation(
       homeLocalDataSource: HomeLocalDataSourceImplementation(),
       homeRemoteDataSource: HomeRemoteDataSourceImplementation(
+        ApiService(
+          Dio(),
+        ),
+      ),
+    ),
+  );
+  getIt.registerSingleton<SearchRepoImplementation>(
+    SearchRepoImplementation(
+      searchLocalDataSource: SearchLocalDataSourceImplementation(),
+      searchRemoteDataSource: SearchRemoteDataSourceImplementation(
         ApiService(
           Dio(),
         ),
