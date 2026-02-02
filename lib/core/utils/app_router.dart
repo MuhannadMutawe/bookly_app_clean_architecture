9b5998ec63107314cee5686d9fd3b91644d1473e
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-02 21:49:59 +0200

    create search books cubit blocProvider to app router.

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,8 +1,13 @@
+import 'package:bookly_clean_arch/core/utils/functions/setup_service_locator.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_clean_arch/features/home/presentation/views/home_view.dart';
+import 'package:bookly_clean_arch/features/search/data/repos/search_repo_implementation.dart';
+import 'package:bookly_clean_arch/features/search/domain/use_cases/fetch_search_books_use_cases.dart';
+import 'package:bookly_clean_arch/features/search/presentation/manger/search_books/search_books_cubit.dart';
 import 'package:bookly_clean_arch/features/search/presentation/views/search_view.dart';
 import 'package:bookly_clean_arch/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -25,18 +30,16 @@ abstract class AppRouter {
           book: state.extra as BookEntity,
         ),
       ),
-      // GoRoute(
-      //   path: kSearchView,
-      //   builder: (context, state) => BlocProvider(
-      //     create: (context) =>
-      //         SearchBooksCubit(getIt.get<SearchRepoImplementation>())
-      //           ..fetchSearchBooksCubit(),
-      //     child: const SearchView(),
-      //   ),
-      // ),
       GoRoute(
         path: kSearchView,
-        builder: (context, state) => SearchView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SearchBooksCubit(
+            FetchSearchBooksUseCases(
+              getIt.get<SearchRepoImplementation>(),
+            ),
+          )..fetchSearchBooks(),
+          child: const SearchView(),
+        ),
       ),
     ],
   );
